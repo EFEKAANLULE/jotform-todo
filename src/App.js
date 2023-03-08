8d1ac4d2c44312f64fa74ae0f36c793b38c32bb7
EFEKAANLULE <101347167+EFEKAANLULE@users.noreply.github.com>
2023-03-08 13:07:23 +0300

    fix: css fix

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ function App({ todos }) {
   console.log(todos);
 
   return (
-    <div style={{ maxWidth: '600px', margin: '0 auto' }}>
+    <div style={{ maxWidth: '50vw', margin: '0 auto' }}>
       <h1>EFE's Project TODO List </h1>
       <ul style={{ marginBottom: '40px' }}>
         {todos.map(todo => (
           <li key={todo.submissionId}>
-            <strong>{todo.name.first} {todo.name.last}</strong> - {todo.description}
+            <strong>{todo.name.first} {todo.name.last}</strong>  -  {todo.description} 
           </li>
         ))}
       </ul>
