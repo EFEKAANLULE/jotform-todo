2bf989139067115a669f824791b3bbbc3cd54520
EFEKAANLULE <101347167+EFEKAANLULE@users.noreply.github.com>
2023-03-17 16:21:41 +0300

    docs: Comments added to explain code

--- src/Api.js
+++ src/Api.js
@@ -3,6 +3,6 @@ export const getSubmissions = async (apiKey,formId) => {
     const response = await fetch(`https://api.jotform.com/form/${formId}/submissions?apiKey=${apiKey}`, {
     });
     const data = await response.json(); // extracr to json format
-    return data.content.filter(submission => submission.status === 'ACTIVE'); // return if keyword 'status' is ACTIVE
+    return data.content.filter(submission => submission.status === 'ACTIVE'); // return  keyword 'status' is ACTIVE
   };
   
--- src/FetchingData.js
+++ src/FetchingData.js
@@ -16,7 +16,7 @@ const FetchingData = () => {
     getSubmissions(apiKey, formId) // from Api.js
       .then(submissions => {
         const tasks = submissions.map((submission, index) => {
-          const { answers } = submission;
+          const { answers } = submission; // we are using {} because answers is a object. 
           const name = (answers[1]?.answer) || ''; 
           const description = (answers[2]?.answer) || '';
           return { id: index, ...answers, name, description };
@@ -31,16 +31,16 @@ const FetchingData = () => {
   }, []);
 
   const handleToggle = (id) => { // responsible for updating the STATE of todos whether is it marked done or not, and arranging arrays from completed todo to  to the completedTodos
-    const updatedTodos = todos.map(todo => { // but it is not doing any actual MOVING, we are doing that part in App.js handletoggle.
-      if (todo.id === id) {
+    const updatedTodos = todos.map(todo => {          // but it is not doing any actual MOVING, we are doing that part in App.js handletoggle.
+      if (todo.id === id) {  // todos.map() creates a new array of todo items with an updated isDone property for the todo item with the specified id.
         return { ...todo, isDone: !todo.isDone };
       }
       return todo;
     });
-    const completedTodo = updatedTodos.find(todo => todo.id === id && todo.isDone);
+    const completedTodo = updatedTodos.find(todo => todo.id === id && todo.isDone); // updatedTodos.find() returns the completed todo item, if there is one, from the updated array.
     if (completedTodo) {
-      setCompletedTodos([...completedTodos, completedTodo]); // add it to the completed 
-      setTodos(updatedTodos.filter(todo => todo.id !== id)); // remove it from in progress 
+      setCompletedTodos([...completedTodos, completedTodo]); // If there is a completed todo item, setCompletedTodos adds it to the completedTodos array, and setTodos updates the todos array to remove the completed todo item.
+      setTodos(updatedTodos.filter(todo => todo.id !== id)); // If there is no completed todo item, setTodos updates the todos array with the updated todo items.
     } else {
       setTodos(updatedTodos);
     }
